2ab11f8b443a7e4173e49d966e890668b130dee3
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2023-11-03 09:45:32 +0530

    Time: 0 ms, Memory: 10.8 MB - LeetHub

---
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <unordered_set>
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s;
+        for (int num : arr) {
+            if (s.count(2 * num) || (num % 2 == 0 && s.count(num / 2))) {
+                return true;
+            }
+            s.insert(num);
+        }
+
+        return false;
+    }
+};
