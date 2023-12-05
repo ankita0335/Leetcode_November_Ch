984b658a537e277abe22f00ceb5df7c9f3076b82
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2023-12-05 14:59:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (13.68%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        return (n-1);
+        int ans=0;
+        int match=1;
+        while(n>1){
+            if(n%2==0){
+                match=n/2;
+                int advance=n/2;
+                n=advance;
+            }
+            else{
+                match=(n-1)/2;
+                int advance=((n-1)/2)+1;
+                n=advance;
+            }
+            ans+=match;
+        }
+        return ans;
     }
 };
