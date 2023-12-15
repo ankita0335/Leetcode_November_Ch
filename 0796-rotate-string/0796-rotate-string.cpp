ab1aed0ce8c0f128d06ac63d363882fbd263f39a
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2023-12-15 13:06:43 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (86.47%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -5,16 +5,8 @@ public:
     bool rotateString(std::string s, std::string goal) {
         int n = s.size();
         int m = goal.size();
-        int flag = 0;
-        string str = s + s;
-        if(n!=m)    return false;
-        for (int i = 0; i < str.size(); i++) {
-            if (str.find(goal) != string::npos) {
-                return true;
-                flag = 1;
-            }
-        }
-        if (flag == 1) return true;
-        return false;
+        if (n != m) return false; 
+        string doubleS = s + s;
+        return doubleS.find(goal) != string::npos;
     }
 };
