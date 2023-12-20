35f981d681b04b081328043cb04c4c9ae88c8981
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2023-12-20 07:47:51 +0530

    Time: 12 ms (59.66%), Space: 47 MB (58.30%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
-    int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(), prices.end());
-        int total = prices[0] + prices[1];
+    int buyChoco(vector<int>& price, int money) {
+        int cheapest1 = INT_MAX, cheapest2 = INT_MAX;
+        for (int p : price) {
+            if (p < cheapest1) {
+                cheapest2 = cheapest1; 
+                cheapest1 = p;         
+            } else if (p < cheapest2) {
+                cheapest2 = p;         
+            }
+        }
+        int total = cheapest1 + cheapest2;
         return (money >= total) ? money - total : money;
     }
 };
