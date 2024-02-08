84bed2718c7b7a7258b652a31a37557b1e4880c8
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-02-09 02:15:57 +0530

    Time: 8 ms (35.33%), Space: 15.9 MB (5.30%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -1,35 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if (head == nullptr)
+        vector<int>v;
+        ListNode* ptr=head;
+        if(head==nullptr)
             return nullptr;
-
-        vector<int> v;
-        ListNode* ptr = head;
-
-        while (ptr != nullptr) {
+        while(ptr!=nullptr)
+        {
             v.push_back(ptr->val);
-            ptr = ptr->next;
+            ptr=ptr->next;
         }
-
-        unordered_map<int, int> mp;
-        vector<int> v1;
-
-        for (int i = 0; i < v.size(); i++) {
+        unordered_map<int,int>mp;
+        vector<int>v1;
+        for(int i=0;i<v.size();i++)
+        {
             mp[v[i]]++;
         }
-
-        for (int i = 0; i < v.size(); i++) {
-            if (mp[v[i]] == 1)
+        for(int i=0;i<v.size();i++)
+        {
+            if(mp[v[i]]==1)
                 v1.push_back(v[i]);
         }
-
-        if (v1.empty()) {
-            return nullptr; 
-        }
+        if(v1.empty())
+            return nullptr;
         ListNode* tmp = new ListNode(0);
         ListNode* t = tmp;
-
         for (int i = 0; i < v1.size(); i++) {
             tmp->val = v1[i];
             if (i < v1.size() - 1) {
@@ -37,7 +42,6 @@ public:
                 tmp = tmp->next;
             }
         }
-
         return t;
     }
 };
